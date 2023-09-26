87954a99adf914a7daf90c14d7db2eda9b333b96
Filippo Bonino <filippo.bonino@edu.unito.it>
2023-09-26 13:53:52 +0200

    first

--- es-array-odd-even.c
+++ es-array-odd-even.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-    char arr[60];
-    int i;
-    for(i = 0; i < 7; i++) {
+    char arr[7];
+    int i, j, len;
+    long ris[7];
+    len = sizeof(arr)/sizeof(arr[0]);
+    for(i = 0; i < len; i++) {
         fgets(arr, sizeof(arr), stdin);
+        ris[i] = strtol(arr, NULL, 10);
     }
-
-    printf("%s \n", arr);
+    for(i = 0; i < len; i = i + 2) {
+        printf("%ld ",  ris[i]);
+    }
+    for(j = 1; j < len; j = j + 2) {
+        printf("%ld ", ris[j]);
+    }
+    printf("\n");
+    return 0;
 }
