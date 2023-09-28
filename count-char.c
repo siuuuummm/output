b10a6381636d019c9781df99cea9998c95b6dc34
Filippo Bonino <filippo.bonino@edu.unito.it>
2023-09-28 15:15:21 +0200

    ciao

--- count-char.c
+++ count-char.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-    char string[60] = {0};
-    int i, len, count = 0;
+    char string[200] = {0};
+    char freq[256] = {0};
+    int i, len;
     len = sizeof(string)/sizeof(string[0]);
     fgets(string, len, stdin);
 
     for(i = 0; i < len; i++) {
-        if(string[i] != '\0') {
-            printf("%d, %d\n", count, string[i]);
+            freq[(int)string[i]]++;
+    }
+    for(i = 32; i < 256; i++) {
+        if(freq[i] != 0) {
+            printf("%d,%c\n", freq[i], i);
         }
     }
     return 0;
