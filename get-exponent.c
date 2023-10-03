ec953346a876f4d7926b1687dc29ebf68a5f3dac
Filippo Bonino <filippo.bonino@edu.unito.it>
2023-10-03 10:25:21 +0200

    ciao

---
+++ get-exponent.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    double esp;
+    int *p, i;
+    scanf("%le", &esp);
+
+    for(*p = esp; *p; p++) {
+        if(*(esp) == 0) {
+            break;
+        }
+        if()
+    }
+
+    printf("%f", esp);
+}
