151f0ee5fa619dfde465cad302df28539dc0ddb9
Filippo Bonino <filippo.bonino@edu.unito.it>
2023-10-27 13:11:04 +0200

    ciao

--- file-shuffle-rows/file-shuffle-rows.c
+++ file-shuffle-rows/file-shuffle-rows.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void) {
+    srand(time(NULL));
+    FILE * read;
+    read = fopen("argv[1].txt", "r");
+    FILE * write;
+    write = fopen("argv[2].txt", "w");
+    int c, count = 0;
+    if(read == NULL) {
+        perror("Impossibile aprire il file");
+        return 1;
+    }
     
+    while((c = fgetc(read)) != EOF) {
+        count++;
+        fputc(rand(),write);
+    }
+
+    fclose(read);
+    fclose(write);
 }
