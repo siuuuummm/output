a4fe69c765f2b85e86021931d6686880d054a0e9
Filippo Bonino <filippo.bonino@edu.unito.it>
2023-11-14 00:10:28 +0100

    commit

--- pizza_prog/libft.c
+++ pizza_prog/libft.c
@@ -44,3 +44,97 @@ void *ft_memccpy(void *dest, const void * src, int c, size_t n) {
     }
     return dest;
 }
+
+void *ft_memmove(void *str1, const void * str2, size_t n) {
+    unsigned char *p = str1;
+    unsigned const char *r = str2;
+    while(n > 0) {
+        *p = *r;
+        p++;
+        r++;
+        n--;
+    }
+    return str1;
+}
+
+void *ft_memchr(const void *str, int c, size_t n) {
+    unsigned char *p = str;
+    while(n > 0) {
+        if(*p != c) {
+            *p = 0;
+            p++;
+        } else {
+            str = &str[1000 - n];
+            return str;
+        }
+        n--;
+    }
+}
+
+int ft_memcmp(const void *str1, const void *str2, size_t n) {
+    unsigned const char *p = str1;
+    unsigned const char *r = str2;
+    int value = 0;
+    while(n > 0) {
+        if(*p == *r) {
+            p++;
+            r++;
+        } else if(*p > *r) {
+            value = 1;
+            return value;
+        } else {
+            value = -1;
+            return value;
+        }
+        n--;
+    }
+    return value;
+}
+
+size_t ft_strlen(const char *str) {
+    const char *p = str;
+    int count = 0;
+    while(*p != 0) {
+        count++;
+        p++;
+    }
+    return count - 1;
+}
+
+size_t ft_strlcpy(char * restrict dest, const char * restrict src, size_t n) {
+    char *p = dest;
+    const char *r = src;
+    int count = 0;
+    while(n > 0) {
+        if(*r != 0 && *r != '\n') {
+            *p = *r;
+            p++;
+            r++;
+            count++;
+        } else if(*p != 0) {
+            p++;
+        }
+        n--;
+    }
+    return count;
+}
+
+size_t ft_strlcat(char *dest, const char *src, size_t n) {
+    char *p = dest;
+    const char *r = src;
+    int count = 0;
+    while(n > 0) {
+        if(*p != 0 && *p != '\n') {
+            p++;
+            count++;
+        }
+        if((*p == 0 || *p == '\n') && *r != 0) {
+            *p = *r;
+            p++;
+            r++;
+            count++;
+        }
+        n--;
+    }
+    return count;
+}
--- pizza_prog/test.c
+++ pizza_prog/test.c
@@ -1,19 +1,44 @@
 #include <stdio.h>
 #include "libft.c"
 
-int main(void) {
-    char str[1000] = {0};
+int main(void) {                //ctrl + shift + frecce
+    char str[13] = {0};
     char frase[1000] = {0};
+    //const char c = 'o';
+    //char * ret;
+    //int val;
+    size_t value;
     printf("Inserire una stringa: ");
     fgets(str, sizeof(str), stdin);
-    ft_memset(str, 'C', 5);
-    printf("Risultato post-memeset: %s", str);
-    ft_bzero(str, 3);
-    printf("Risultato post-bzero: %s", str);
+    //ft_memset(str, 'C', 5);
+    //printf("Risultato post-memeset: %s", str);
+    //ft_bzero(str, 3);
+    //printf("Risultato post-bzero: %s", str);
+    printf("Inserire seconda stringa: ");
     fgets(frase, sizeof(frase), stdin);
-    ft_memcpy(str, frase, sizeof(frase));
-    printf("Risultato post-memcpy: %s", str);
-    fgets(frase, sizeof(frase), stdin);
-    ft_memccpy(str, frase, 'o', sizeof(frase));
-    printf("Risultato post-memccpy: %s", str);
+    //ft_memcpy(str, frase, sizeof(frase));
+    //printf("Risultato post-memcpy: %s", str);
+    //fgets(frase, sizeof(frase), stdin);
+    //ft_memccpy(str, frase, 'o', sizeof(frase));
+    //printf("Risultato post-memccpy: %s", str);
+    //ft_memmove(str, frase, 8);
+    //printf("Risultato post-meemmove: %s", str);
+    //ret = ft_memchr(str, c, sizeof(str));
+    //printf("Risultato post-memchr: %s", ret);
+    //val = ft_memcmp(str, frase, 10);
+    //if(val > 0) {
+    //    printf("str2 è minore di str1\n");
+    //} else if(val < 0) {
+    //    printf("str1 è minore di str2\n");
+    //} else {
+    //    printf("str1 è uguale a str2\n");
+    //}
+    //val = ft_strlen(str);
+    //printf("Risultato post-strlen: %d\n", val);
+    //value = ft_strlcpy(str, frase, sizeof(str));
+    //printf("Risultato post-strlcpy: %s",str);
+    //printf(" with length %ld\n", value);
+    value = ft_strlcat(str, frase, sizeof(str));
+    printf("Risultato post-strlcat: %s", str);
+    printf("with length %ld", value);
 }
