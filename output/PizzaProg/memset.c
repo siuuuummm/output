3341dd4fd36a999461c6ea8da0c0809f3463d307
Pippo <116815328+siuuuummm@users.noreply.github.com>
2023-11-14 00:14:12 +0100

    Update memset.c

--- output/PizzaProg/memset.c
+++ output/PizzaProg/memset.c
@@ -1,12 +1,1 @@
-#include <stdio.h>
-#include "libft.h"
 
-void *ft_memset(void *str, int c, size_t n) {
-    str = (int[10000000]){};
-    for(int i = 0; n > 0; i++) {
-        str[i] = c;
-        n--;
-    }
-    printf("ma che stai a fa??");
-    return 0;
-}
